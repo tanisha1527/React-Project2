b794a6e26e1570c81cada4b53d96053d0a80fb34
tanisha1527 <vermatanisha620@gmail.com>
2024-11-22 02:59:50 +0530

    Add new file

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -9,6 +9,15 @@ const Todo = () => {
          <img className='w-8' src={todo_icon} alt="" />
          <h1 className='text-3xl font-semibold'>To-Do List</h1>
     </div>
+     
+    <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+        <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
+        <button className='border-none rounded-full bg-green-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+    </div>
+
+    <div>
+        
+    </div>
 
     </div>
   )
