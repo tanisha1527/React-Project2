055f5d0e0815e2fb5345493e724347c00943e647
tanisha1527 <vermatanisha620@gmail.com>
2024-11-22 02:47:51 +0530

    create title

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import todo_icon from '../assets/todo_icon.png'
 
 const Todo = () => {
   return (
-    <div>
-        <h1>Todo</h1>
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+      
+    <div className='flex items-center mt-7 gap-2'>
+         <img className='w-8' src={todo_icon} alt="" />
+         <h1 className='text-3xl font-semibold'>To-Do List</h1>
+    </div>
+
     </div>
   )
 }
