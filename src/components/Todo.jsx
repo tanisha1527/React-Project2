ecd229b94d0af91d09c99001ca45aead35ce52b1
tanisha1527 <vermatanisha620@gmail.com>
2024-11-22 16:58:14 +0530

    commit changes and complete toggle functionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from './TodoItems'
 
@@ -30,6 +30,21 @@ const deleteTodo = (id)=>{
       })
 }
 
+const toggle = (id)=>{
+    setTodoList((prevTodos)=>{
+       return prevTodos.map((todo)=>{
+         if(todo.id === id){
+            return {...todo, isComplete: !todo.isComplete}
+         }
+         return todo;
+       })
+    })
+}
+
+useEffect(()=>{
+    console.log(todoList);
+},[todoList])
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
       
@@ -40,12 +55,12 @@ const deleteTodo = (id)=>{
      
     <div className='flex items-center my-7 bg-gray-200 rounded-full'>
         <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
-        <button onClick={add} className='border-none rounded-full bg-green-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+        <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
     </div>
 
     <div>
      {todoList.map((item,index)=>{
-         return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} />
+         return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle} />
      })}
 
     </div>
