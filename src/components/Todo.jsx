8c15fca9f7bb8b5332c571dc8d9c17e5f18c4670
tanisha1527 <vermatanisha620@gmail.com>
2025-01-06 20:20:42 +0530

    make todo-list page responsive

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -46,16 +46,16 @@ useEffect(()=>{
 },[todoList])
 
   return (
-    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 sm:p-5 min-h-[550px] sm:min-h-[400px]: rounded-xl'>
       
     <div className='flex items-center mt-7 gap-2'>
          <img className='w-8' src={todo_icon} alt="" />
-         <h1 className='text-3xl font-semibold'>To-Do List</h1>
+         <h1 className='text-3xl font-semibold text-center sm:text-left'>To-Do List</h1>
     </div>
      
     <div className='flex items-center my-7 bg-gray-200 rounded-full'>
         <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
-        <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+        <button onClick={add} className='border-none rounded-full bg-orange-600 w-full sm:w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
     </div>
 
     <div>
